6dd36e908d53a365bf108b136de41cf5cd54d632
Juampa09 <juanaristi.david9@gmail.com>
2025-11-13 08:15:40 -0500

    Update BUTTONS.py

--- pages/BUTTONS.py
+++ pages/BUTTONS.py
@@ -62,7 +62,7 @@ values = st.slider('Selecciona el rango de valores',0.0, 100.0)
 st.write('Values:', values)
 
 if st.button('Enviar valor analógico'):
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("DOOM")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)   
     message =json.dumps({"Analog": float(values)})
